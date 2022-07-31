3a4468bd929c318003cfe360e1927811adcff350
Rober <zeniquelrober@gmail.com>
2022-07-30 22:05:34 -0300

    Disparos y efectos visuales

--- Juego/Naves/Player/Player.gd
+++ Juego/Naves/Player/Player.gd
@@ -6,11 +6,23 @@ extends RigidBody2D
 export var potencia_motor:int = 12
 export var potencia_rotacion:int = 280
 
+## Atributos onready
+onready var canion:Canion = $Canion
+onready var laser:RayoLaser = $LaserBeam2D
+
 ## Atributos
 var empuje:Vector2 = Vector2.ZERO
 var dir_rotacion = 0
 
 ## Metodos
+func _unhandled_input(event: InputEvent) -> void:
+	# Disparo rayo
+	if event.is_action_pressed("disparo_secundario"):
+		laser.set_is_casting(true)
+	
+	if event.is_action_released("disparo_secundario"):
+		laser.set_is_casting(false)
+
 func _integrate_forces(state: Physics2DDirectBodyState) -> void:
 	apply_central_impulse(empuje.rotated(rotation))
 	apply_torque_impulse(dir_rotacion * potencia_rotacion)
@@ -33,4 +45,19 @@ func player_input() -> void:
 		dir_rotacion -= 1
 	elif Input.is_action_pressed("rotar_horario"):
 		dir_rotacion += 1
+	
+	## Diaparo
+	if Input.is_action_pressed("disparo_principal"):
+		canion.set_esta_disparando(true)
+		
+	if Input.is_action_just_released("disparo_principal"):
+		canion.set_esta_disparando(false)
+		
+
+
+
+
+
+
+
 	
